85f445d7e606d9d091549c6766c22cd55fddd200
Cris <cristiano.vu@ewoosoft.com>
2024-04-08 13:53:40 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Test from '~/components/Test'
+
+function App() {
+  return (
+    <>
+      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
+      <Test />
+    </>
+  )
+}
+
+export default App
