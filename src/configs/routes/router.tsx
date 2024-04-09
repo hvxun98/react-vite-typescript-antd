e1bbc156d997514330473c3f4f1c2a683d34e6cd
Cris <cristiano.vu@ewoosoft.com>
2024-04-09 16:01:45 +0700

    test router

--- src/configs/routes/router.tsx
+++ src/configs/routes/router.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom'
 import Page500 from '~/pages/500'
 import Page404 from '~/pages/404'
 import Home from '~/pages/Home'
+import Login from '~/pages/Login'
 
 export const router = createBrowserRouter([
   {
@@ -9,7 +10,11 @@ export const router = createBrowserRouter([
     element: <Home />,
     errorElement: <Page500 />
   },
-
+  {
+    path: '/login',
+    element: <Login />,
+    errorElement: <Page500 />
+  },
   {
     path: '*',
     element: <Page404 />
